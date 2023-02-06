55759a2c0f91f2ff9378b8bb606725b5073367c5
Vsevolod Lutovinov <klopp@yandex.ru>
2023-02-06 09:20:41 +0300

    clone() issues

--- Resource/Data.pm
+++ Resource/Data.pm
@@ -3,7 +3,9 @@ package Resource::Data;
 # ------------------------------------------------------------------------------
 use Modern::Perl;
 
+use Carp qw/cluck/;
 use Data::Clone qw/clone/;
+use Scalar::Util qw/blessed/;
 
 use lib q{..};
 use Resource::Base;
@@ -21,6 +23,7 @@ sub new
                     вложенности 
                     может быть blessed, желательно с методом clone()
     В {params} МОЖЕТ быть:
+        {quiet} не выводить предупреждения
         {id}
     Структура после полной инициализации:
         {id}
@@ -31,6 +34,11 @@ sub new
 =cut    
 
     my ( $class, $params ) = @_;
+    my $blessed = blessed ${ $params->{source} };
+    if ( $blessed && !$blessed->can('clone') ) {
+        $params->{quiet}
+            or cluck sprintf 'Data :: %s->clone() is not defined, object cloning may be inaccurate!', $blessed;
+    }
     return $class->SUPER::new($params);
 }
 
@@ -42,17 +50,24 @@ sub check_params
     if (   ref $self->{params}->{source} ne 'REF'
         && ref $self->{params}->{source} ne 'SCALAR' )
     {
-
         return 'ref {params}->{source} is not REF or SCALAR';
     }
     return;
 }
 
 # ------------------------------------------------------------------------------
+sub _clone
+{
+    my ( $self, $src ) = @_;
+    ( blessed $src && $src->can('clone') ) and return $src->clone;
+    return clone $src;
+}
+
+# ------------------------------------------------------------------------------
 sub create_backup_copy
 {
     my ($self) = @_;
-    $self->{backup} = clone( ${ $self->{params}->{source} } );
+    $self->{backup} = $self->_clone( ${ $self->{params}->{source} } );
     return;
 }
 
@@ -68,7 +83,7 @@ sub delete_backup_copy
 sub create_work_copy
 {
     my ($self) = @_;
-    $self->{work} = clone( ${ $self->{params}->{source} } );
+    $self->{work} = $self->_clone( ${ $self->{params}->{source} } );
     return;
 }
 
@@ -84,7 +99,7 @@ sub delete_work_copy
 sub commit
 {
     my ($self) = @_;
-    $self->{work} and ${ $self->{params}->{source} } = clone( $self->{work} );
+    $self->{work} and ${ $self->{params}->{source} } = $self->_clone( $self->{work} );
     return;
 }
 
@@ -92,7 +107,7 @@ sub commit
 sub rollback
 {
     my ($self) = @_;
-    $self->{backup} and ${ $self->{params}->{source} } = clone( $self->{backup} );
+    $self->{backup} and ${ $self->{params}->{source} } = $self->_clone( $self->{backup} );
     return;
 }
 
